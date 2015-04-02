9fce90e2260ab33bcf1cfd3148b3b9ef5cb0a7e3
sreiss <simon.reiss57@gmail.com>
2015-04-02 17:55:10 +0200

    parallax

--- client/web/src/app/parallax/arch-parallax.directive.js
+++ client/web/src/app/parallax/arch-parallax.directive.js
@@ -1,11 +1,26 @@
 'use strict'
 angular.module('archCarto')
   .directive('archParallax', [
-    function() {
+    '$window',
+    function($window) {
       return {
         restrict: 'A',
         link: function(scope, element, attributes) {
-          debugger;
+          var windowElement = angular.element($window);
+          var speed = parseInt(attributes.archParallax, 10) || 4;
+
+          var parallax = function() {
+            var yPos = -(windowElement.scrollTop() / speed);
+
+            // Put together our final background position
+            var coords = '50% '+ yPos + 'px';
+
+            // Move the background
+            element.css({ backgroundPosition: coords });
+          };
+
+          parallax();
+          windowElement.on('scroll', parallax);
         }
       };
     }
