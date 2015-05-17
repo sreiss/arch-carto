482b00cc3a77178762b91becb2fbc13c36a98e1c
sreiss <s.reiss@outlook.fr>
2015-05-17 02:01:58 +0200

    Prod urls

--- client/web/src/app/constants/http.constant.js
+++ client/web/src/app/constants/http.constant.js
@@ -1,9 +1,18 @@
 'use strict'
 angular.module('archCarto')
   .constant('httpConstant', {
+    /* PROD */
+     apiUrl: 'http://acrobatt-vm11.psi.ad.unistra.fr:3022',
+     loginUrl: 'http://acrobatt-vm12.psi.ad.unistra.fr:3010',
+     casUrl: 'http://acrobatt-vm12.psi.ad.unistra.fr:3021',
+     clientName: 'ARCH-CARTO',
+     clientRedirectUri : 'http://acrobatt-vm11.psi.ad.unistra.fr/#/map'
+     /**/
+    /* DEV
     apiUrl: 'http://localhost:3022',
     loginUrl: 'http://localhost:3010',
     casUrl: 'http://localhost:3021',
     clientName: 'ARCH-CARTO',
     clientRedirectUri : 'http://localhost:3012/#/map'
+    */
   });
