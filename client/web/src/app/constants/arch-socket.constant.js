e4a01c3df314a7e61d9f51998de5a50c9d36fc99
sreiss <s.reiss@outlook.fr>
2015-05-23 14:09:11 +0200

    PROD url

--- client/web/src/app/constants/arch-socket.constant.js
+++ client/web/src/app/constants/arch-socket.constant.js
@@ -1,5 +1,8 @@
 'use strict';
 angular.module('archCarto')
   .constant('archSocketConstant', {
+    /* DEV
     url: 'ws://localhost:3022'
+    */
+    url: 'ws://acrobatt-vm11.psi.ad.unistra.fr'
   });
