1bea797ac976790bc921a34d8d924591dda9e9e2
sreiss <simon.reiss57@gmail.com>
2015-05-28 08:50:13 +0200

    Server media controller return changed

--- server/plugins/map/controllers/media.js
+++ server/plugins/map/controllers/media.js
@@ -1,13 +1,19 @@
 module.exports = function(mediaService) {
 
     return {
-        save: function(req, res) {
+        save: function(req, res, next) {
             mediaService.save({
                     files: req.files,
                     body: req.body
                 })
                 .then(function(media) {
-                    res.json(media);
+                    res.json({
+                        message: 'MEDIA_SAVED',
+                        value: media
+                    });
+                })
+                .catch(function(err) {
+                    next(err);
                 });
         }
     };
