a64bb17a149cf9b2c3f116c98e46275aa7a937e4
loic <loic.masson@etu.unistra.fr>
2015-06-06 20:37:48 +0200

    Simplify in a good place OKLM

--- client/web/src/components/gpx/arch-gpx.service.js
+++ client/web/src/components/gpx/arch-gpx.service.js
@@ -1,28 +1,36 @@
 'use strict'
 angular.module('archCarto')
-  .factory('archGpxService', function($http, httpConstant, $q,archHttpService) {
-    var _gpxUrl = httpConstant.cartoServerUrl + '/map/gpx';
+    .factory('archGpxService', function($http, httpConstant, $q,archHttpService) {
+      var _gpxUrl = httpConstant.cartoServerUrl + '/map/gpx';
 
-    return {
-      getTrace: function(params) {
+      return {
+        getTrace: function(params) {
           return archHttpService.get(_gpxUrl, {params: params});
-      },
-      simplifyTrace: function(geoJson) {
-        //console.log()
-        var deferred = $q.defer();
-        //debugger;
-        var coordonnees = [];
-        for (var i = 0; i < geoJson.features[0].geometry.coordinates.length; i++) {
-          coordonnees.push({
-            0: geoJson.features[0].geometry.coordinates[i][0],
-            1: geoJson.features[0].geometry.coordinates[i][1],
-            2: geoJson.features[0].geometry.coordinates[i][2]
-          });
-        }
-        //console.log(coordonnees.x);
-        deferred.resolve(simplify(coordonnees,0.0001));
-        return deferred.promise;
+        },
+        simplifyTrace: function(geoJson) {
+          //console.log()
+          var deferred = $q.defer();
+          var coordonnees = [];
+          for (var i = 0; i < geoJson.features[0].geometry.coordinates.length; i++) {
+            coordonnees.push({
+              x: geoJson.features[0].geometry.coordinates[i][0],
+              y: geoJson.features[0].geometry.coordinates[i][1],
+              z: geoJson.features[0].geometry.coordinates[i][2]
+            });
+          }
+          var newCoordinates = simplify(coordonnees,0.0001);
+          coordonnees = [];
+          for (var i = 0; i < newCoordinates.length; i++) {
+            coordonnees.push({
+              0: newCoordinates[i]['x'],
+              1: newCoordinates[i]['y'],
+              2: newCoordinates[i]['z']
+            });
+          }
+          //console.log(coordonnees);
+          deferred.resolve(coordonnees);
+          return deferred.promise;
 
-      }
-    };
-  });
+        }
+      };
+    });
