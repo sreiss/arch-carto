18520a23c70ae62021b3c614d239db72fe8b55d3
Maxime RAUCH <maxime@rauch.fr>
2015-06-12 16:21:38 +0200

    Add setDefaultConfig + setDefaultData functions.

--- client/web/src/app/http/arch-http.service.js
+++ client/web/src/app/http/arch-http.service.js
@@ -1,42 +1,93 @@
 'use strict';
 angular.module('archCarto')
-  .factory('archHttpService', function($http, $q) {
+  .factory('archHttpService', function($http, $q, $cookieStore, $base64) {
     return {
-      get: function(url, config) {
+      get: function(url, config)
+      {
         var deferred = $q.defer();
-        config = config || {};
-        $http.get(url, config)
-          .success(function(result) {
-            deferred.resolve(result);
-          })
-          .error(function(err) {
-            deferred.reject(err);
-          });
+        config = this.setDefaultConfig(config);
+
+        $http.get(url, config).success(function(result)
+        {
+          deferred.resolve(result);
+        })
+        .error(function(err)
+        {
+          deferred.reject(err);
+        });
+
         return deferred.promise;
       },
-      post: function(url, data, config) {
+
+      post: function(url, data, config)
+      {
         var deferred = $q.defer();
-        config = config || {};
-        $http.post(url, data, config)
-          .success(function(result) {
-            deferred.resolve(result);
-          })
-          .error(function(err) {
-            deferred.reject(err);
-          });
+        config = this.setDefaultConfig(config);
+        data = this.setDefaultData(data);
+
+        $http.post(url, data, config).success(function(result)
+        {
+          deferred.resolve(result);
+        })
+        .error(function(err)
+        {
+          deferred.reject(err);
+        });
+
         return deferred.promise;
       },
-      delete: function(url, config) {
+
+      delete: function(url, config)
+      {
         var deferred = $q.defer();
-        config = config || {};
-        $http.delete(url, config)
-          .success(function(result) {
-            deferred.resolve(result);
-          })
-          .error(function(err) {
-            deferred.reject(err);
-          });
+        config = this.setDefaultConfig(config);
+
+        $http.delete(url, config).success(function(result)
+        {
+          deferred.resolve(result);
+        })
+        .error(function(err)
+        {
+          deferred.reject(err);
+        });
+
         return deferred.promise;
+      },
+
+      setDefaultConfig: function(config)
+      {
+        var config = config || {};
+        var clientId = $cookieStore.get('clientId');
+        var clientSecret = $cookieStore.get('clientSecret');
+
+        config.headers =
+        {
+          "Authorization" : 'Basic ' + $base64.encode(clientId + ':' + clientSecret),
+          "Content-Type" : "application/x-www-form-urlencoded"
+        };
+
+        config.transformRequest = function(obj)
+        {
+          var str = [];
+
+          for(var p in obj)
+          {
+            str.push(encodeURIComponent(p) + "=" + encodeURIComponent(obj[p]));
+          }
+
+          return str.join("&");
+        };
+
+        return config;
+      },
+
+      setDefaultData: function(data)
+      {
+        var data = data || {};
+        var token = $cookieStore.get('token');
+        data.token = token.access_token || null;
+
+        return data;
       }
     };
   });
--- server/plugins/users/middlewares/authMiddleware.js
+++ server/plugins/users/middlewares/authMiddleware.js
@@ -1,23 +1,26 @@
 var passport = require('passport'),
     BearerStrategy = require('passport-http-bearer');
 
-module.exports = function() {
-    /**
-     * @module Oauth2.0 middleware to get the current user from the given token.
-     */
-    passport.use(new BearerStrategy(
-        function (token, next) {
-            User.findOne({token: token}, function (err, user) {
-                if (err) {
-                    return next(err);
-                }
-                if (!user) {
-                    return next(null, false);
-                }
+module.exports = function(User)
+{
+    passport.use(new BearerStrategy(function (token, next)
+    {
+        User.findOne({token: token}, function (err, user)
+        {
+            if (err)
+            {
+                return next(err);
+            }
+            else if(!user)
+            {
+                return next(null, false);
+            }
+            else
+            {
                 return next(null, user, {scope: 'read'});
-            });
-        }
-    ));
+            }
+        });
+    }));
 
     return {
         authenticate: passport.authenticate('bearer', {session: false})
