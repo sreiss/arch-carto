909f595f8366a4e4f2793bf049d53d63bd2ec2ba
Maxime RAUCH <maxime@rauch.fr>
2015-06-17 12:58:24 +0200

    Fix PROD contants.

--- client/web/src/app/constants/http.constant.js
+++ client/web/src/app/constants/http.constant.js
@@ -5,17 +5,17 @@ angular.module('archCarto')
     casClientUrl: 'http://acrobatt-vm12.psi.ad.unistra.fr:3020',
     casServerUrl: 'http://acrobatt-vm12.psi.ad.unistra.fr:3020',
     coreServerUrl: 'http://acrobatt-vm11.psi.ad.unistra.fr:3021',
-   // cartoClientUrl: 'http://acrobatt-vm11.psi.ad.unistra.fr:3022',
-  //  cartoServerUrl: 'http://acrobatt-vm11.psi.ad.unistra.fr:3022',
-   // clientRedirectUri : 'http://acrobatt-vm11.psi.ad.unistra.fr:3022',
+    cartoClientUrl: 'http://acrobatt-vm11.psi.ad.unistra.fr:3022',
+    cartoServerUrl: 'http://acrobatt-vm11.psi.ad.unistra.fr:3022',
+    clientRedirectUri : 'http://acrobatt-vm11.psi.ad.unistra.fr:3022',
 
-    /* DEV */
-  //  casClientUrl: 'http://localhost:3010',
-  //  casServerUrl: 'http://localhost:3020',
-   // coreServerUrl: 'http://localhost:3021',
+    /* DEV
+    casClientUrl: 'http://localhost:3010',
+    casServerUrl: 'http://localhost:3020',
+    coreServerUrl: 'http://localhost:3021',
     cartoClientUrl: 'http://localhost:3012',
     cartoServerUrl: 'http://localhost:3022',
-    clientRedirectUri : 'http://localhost:3012',
+    clientRedirectUri : 'http://localhost:3012', */
 
     signupType: {
       name: 'CARTO',
