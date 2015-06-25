ba5bb9a3cb206d7e61dbd73951208734a9f43dd7
loic <loic.masson@etu.unistra.fr>
2015-06-25 22:44:53 +0200

    Length troncon

--- client/web/src/components/gpx/arch-map-trace-liste.directive.js
+++ client/web/src/components/gpx/arch-map-trace-liste.directive.js
@@ -42,15 +42,7 @@ angular.module('archCarto')
 
         //debugger;
         scope.displayTrace = function(geoJson){
-          //debugger;
 
-          //archInfoService.getDGeoJ(geoJson).then(function(distance){
-          //  console.log(distance);
-          //});
-          //archInfoService.getDistanceGeoJ(geoJson).then(function(distance){
-          //  console.log(distance);
-          //});
-          //archInfoService.getD
           if(archMap.removeControl('el'))
           {
             archMap.removeControl('el');
@@ -63,42 +55,6 @@ angular.module('archCarto')
             archMap.drawElevation(geoJson);
 
           }
-          //archMap.getMap().then(function (map) {
-          //TO DO clear el to have only one chart
-          //map.removeControl(el);
-          //var el = L.control.elevation({
-          //  position: "bottomleft",
-          //  theme: "steelblue-theme", //default: lime-theme
-          //  width: 600,
-          //  height: 125,
-          //  margins: {
-          //    top: 10,
-          //    right: 20,
-          //    bottom: 30,
-          //    left: 50
-          //  },
-          //  useHeightIndicator: true, //if false a marker is drawn at map position
-          //  interpolation: "linear", //see https://github.com/mbostock/d3/wiki/SVG-Shapes#wiki-area_interpolate
-          //  hoverNumber: {
-          //    decimalsX: 3, //decimals on distance (always in km)
-          //    decimalsY: 0, //deciamls on height (always in m)
-          //    formatter: undefined //custom formatter function may be injected
-          //  },
-          //  xTicks: undefined, //number of ticks in x axis, calculated by default according to width
-          //  yTicks: undefined, //number of ticks on y axis, calculated by default according to height
-          //  collapsed: true    //collapsed mode, show chart on click or mouseover
-          //});
-          //console.log(el);
-          //debugger;
-          ////el.clearLayers();
-          //el.addTo(map);
-          //var layer = L.geoJson(geoJson, {
-          //  onEachFeature: el.addData.bind(el)
-          //  //working on a better solution
-          //}).addTo(map);
-          //map.fitBounds(layer.getBounds());
-
-          //});
         }
 
         scope.editTrace = function(geoJson){
--- client/web/src/components/path/arch-path-details-popup.directive.js
+++ client/web/src/components/path/arch-path-details-popup.directive.js
@@ -1,16 +1,19 @@
 'use strict'
 angular.module('archCarto')
-  .directive('archPathDetailsPopup', function($rootScope, archPathService, httpConstant) {
+  .directive('archPathDetailsPopup', function($rootScope, archPathService, httpConstant, archInfoService) {
     return {
       restrict: 'E',
       templateUrl: 'components/path/arch-path-details-popup.html',
       link: function(scope, element, attributes) {
         scope.apiUrl = httpConstant.cartoServerUrl;
-
         var refresh = function() {
           archPathService.get(scope.id)
             .then(function (result) {
               scope.path = result.value;
+              archInfoService.getDistance(result.value)
+                .then(function(distance){
+                  scope.length = distance;
+                });
             });
         };
         refresh();
